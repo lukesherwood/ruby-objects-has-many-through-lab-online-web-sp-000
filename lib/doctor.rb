0537641e6279dba94adfe5cb5d33106347c50ecd
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 17:32:15 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+  @@all =[]
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+  
+  
+  
+end
