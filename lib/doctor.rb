99ce4f07134af77445faf8cc129893ad3cde9307
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 18:06:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,8 +11,8 @@ class Doctor
     @@all
   end
   
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
   end
   
    def appointments
