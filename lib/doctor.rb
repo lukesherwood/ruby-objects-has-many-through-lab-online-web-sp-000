487209c69406bce57c59e3db56514a9e0e3dd7f6
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 18:13:23 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  @@all =[]
+  attr_accessor :name, :doctor
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(patient, date, self)
+  end
+  
+   def appointments
+   Appointment.all.select do |s| 
+     s.doctor == self
+   end
+ end
+ 
+ def patients
+   self.appointments.collect {|s| s.patient}
+ end
+  
+end
---
+++ lib/patient.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(doctor, self, date)
+  end
+  
+  def appointments
+   Appointment.all.select do |s| 
+     s.patient == self
+   end
+ end
+ 
+ def doctors
+   self.appointments.collect {|s| s.doctor}
+ end
+  
+  
+end
